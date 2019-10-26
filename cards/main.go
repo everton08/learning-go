e5943df888a65c557e0d4843bce1d2bdb2de4029
Everton Nunes <everton.code@gmail.com>
2019-10-26 11:04:30 -0300

    :sparkles: Add shuffle func

--- cards/main.go
+++ cards/main.go
@@ -1,11 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := newDeck()
-	fmt.Println(cards.toString())
-	cards.saveToFile("pepo_cards")
+	cards.shuffle()
+	cards.print()
+	// fmt.Println(cards.toString())
+	// cards.saveToFile("pepo_cards")
 
 	// new deck from file
 	// cards := newDeckFromFile("pepo_cards")
